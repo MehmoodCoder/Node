020b03b5b01a986160501bdd574da332e81b2963
Mehmood Hassan <mehmood.mh56@gmail.com>
2026-07-23 16:40:48 +0500

    Minner changes

--- Learning/Server/index.js
+++ Learning/Server/index.js
@@ -1,31 +1,33 @@
-import http from 'http'
-import fs from 'fs'
-
+import http from "http";
+import fs from "fs";
+import url from "url";
 
 const Server = http.createServer((req, res) => {
-    const log = `${Date.now().toLocaleString()} ${req.url} ${req.signal} New Request Recieved.\n`
+
+  const log = `${Date.now().toLocaleString()} ${req.url} New Request Recieved.\n`;
+  if (res.url !== "/favicon.ico") {
     fs.appendFile("log.txt", log, (err, data) => {
-        if (err) {
-            console.log("Error :", err);
-            
-        } else {
-            switch (req.url) {
-                case '/':
-                    res.end("Hello from server HomePage");
-                    break;
-                case "/about":
-                    res.end("Hello, I'm Mehmood Coder");
-                    break
-                default:
-                    res.end("404 Error Not Found")
-                    break;
-            }
-            
+      if (err) {
+        console.log("Error :", err);
+      } else {
+        switch (req.url) {
+          case "/":
+            res.end("Hello from server HomePage");
+            break;
+          case "/about":
+            res.end("Hello, I'm Mehmood Coder");
+            break;
+          default:
+            res.end("404 Error Not Found");
+            break;
         }
-    })
-    console.log(req.headers);
-    
-})
+      }
+      console.log(req.headers);
+    });
+  } else {
+    res.end("");
+  }
+  
+});
 
-
-Server.listen(8000, () => console.log("server started"))
+Server.listen(8000, () => console.log("server started"));
