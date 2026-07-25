440d3be19ddddd5021841f1c3b078d6fb3397d66
Mehmood Hassan <mehmood.mh56@gmail.com>
2026-07-25 23:17:40 +0500

    Connect DB with express Add omplete guide in the form of comments node how to connect your DB with express and in url after port add name of your db in it
    Create Schema and & model on that Schema
    
    and will do work with mongoose instead of >json or .txt files

--- Learning/RestAPI/index.js
+++ Learning/RestAPI/index.js
@@ -6,6 +6,76 @@ import mongoose from "mongoose";
 const app = express();
 const port = 3000;
 
+// connect with DB
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/my-first-db")
+  .then(() => console.log("DB is connected Successfully"))
+  .catch((e) => console.log("DB Error : ", e));
+
+/* 
+
+// 1. Display all available databases on the MongoDB server
+show dbs
+
+// 2. Switch the active context to a specific database (or create it if it doesn't exist)
+use admin
+
+// 3. List all collections (tables) inside the currently active database
+show collections
+
+// 4. Query and retrieve all documents (records) from a specified collection
+db.collection_name.find({})
+
+// 1. Create and switch to a new database (database is physically created once data is inserted)
+use myNewDatabase
+
+// 2. Insert a single document (record) into a collection
+db.users.insertOne({ name: "Mehmood", role: "Developer" })
+
+// 3. Insert multiple documents into a collection at once using an array
+db.users.insertMany([{ name: "Ali", age: 22 }, { name: "Sara", age: 24 }])
+
+// 4. Retrieve and view all inserted documents in the collection
+db.users.find()
+
+// 5. Retrieve all documents with readable indenting/formatting
+db.users.find().pretty()
+
+// 5. Exit the interactive MongoDB Shell session and return to the system terminal
+.exit
+
+*/
+
+// Schema
+
+const userSchema = new mongoose.Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  gender: {
+    type: String,
+  },
+  jobtitle: {
+    type: String,
+    required: true,
+  },
+});
+
+// Model of Schema
+
+const User = mongoose.model("user", userSchema);
+
 /*
  * NOTE: Development Server Setup
  * Installed 'nodemon' (npm i nodemon) for hot-reloading on code changes.
